e3e8b96cdd89c6aa3c911c5e3801d886bd27f634
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2023-11-28 19:49:53 +0400

    Passing Data Via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App(){
-  return <div><ListGroup /></div>
+
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return <div><ListGroup items={items} heading="List" /></div>
 }
 
 export default App;
