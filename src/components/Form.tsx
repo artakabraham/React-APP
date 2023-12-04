79aea2bf7189531ec186cc05e831c7d5018d98f0
Artak <abrahamyan@outlook.com>
2023-12-05 00:00:28 +0400

    Add schema based form validation with Zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,42 +1,60 @@
-import {FieldValues, useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData{
-    name: string;
-    age: number;
-}
+const schema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "The name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-    const {register, handleSubmit, formState : { errors } } = useForm<FormData>();
-    
-    const onSubmit = (data : FieldValues) => {
-        console.log(data);
-    }
-    
-    return(
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor='name' className='form-label'>Name</label>
-                <input
-                    {...register('name', {required : true, minLength: 3})}
-                    id='name' 
-                    type='text' 
-                    className='form-control' />
-                    { errors.name?.type === 'rquired' && <p className='text-danger'>The name field is required</p>}
-                    { errors.name?.type === 'minLength' && <p className='text-danger'>The name must be at least 3 characters.</p>}
-            </div>
-            <div>
-                <label htmlFor='age' className='form-label'>Age</label>
-                <input 
-                    {...register('age')}
-                    id='age' 
-                    type='number' 
-                    className='form-control' 
-                    />
-            </div>
-            <button className='btn btn-primary' type='submit'>Submit</button>
-        </form>
-    )
-}
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
