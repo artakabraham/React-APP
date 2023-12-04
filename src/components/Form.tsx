ddad2525c2c739193f2ab7717ff08c16224c73ed
Artak <abrahamyan@outlook.com>
2023-12-05 00:10:28 +0400

    Make button disabled when form is not valid.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,7 +17,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
@@ -50,7 +50,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button className="btn btn-primary" type="submit" disabled={!isValid}>
         Submit
       </button>
     </form>
