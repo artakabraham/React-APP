a49bb588a4b6af1e1b69818f2ac1776fec64f9b7
Artak <abrahamyan@outlook.com>
2023-12-06 23:08:59 +0400

    Initialize expense tracker module

---
+++ src/expense-tracker/ExpenseList.tsx
@@ -0,0 +1,15 @@
+const ExpenseList = () => {
+  return (
+    <table>
+      <thead>
+        <th>Description</th>
+        <th>Amount</th>
+        <th>Category</th>
+        <th></th>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+
+export default ExpenseList;
