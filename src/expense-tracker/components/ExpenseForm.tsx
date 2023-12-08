880a6f069f83fd26d67aa9af2283e2a88239f07e
Artak Abrahamyan <Artak.Abrahamyan@medfarsolutions.com>
2023-12-08 16:05:33 +0400

    Add form submit

--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -4,6 +4,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import categories from "../categories";
 
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
 const schema = z.object({
   description: z.string().min(3, { message: "Description should be at least 3 characters." }).max(50, { message: "Description should be maximum 50 characters." }),
   amount: z.number({ invalid_type_error: "Amount is required." }).min(0.01).max(100_000),
@@ -16,12 +20,15 @@ const schema = z.object({
 
 type ExpenseFormData = z.infer<typeof schema>;
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSubmit }: Props) => {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
   return (
-    <form onSubmit={handleSubmit(data => console.log(data))}>
+    <form onSubmit={handleSubmit(data => {
+      onSubmit(data);
+      reset();
+    })}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
@@ -41,6 +48,7 @@ const ExpenseForm = () => {
           Category
         </label>
         <select {...register("category")} id="category" className="form-select">
+          <option value=""></option>
           {categories.map((category) => (
             <option key={category} value={category}>
               {category}
