2e4b0aa394d377ae9d0c773c58c76d128b953075
Artak <abrahamyan@outlook.com>
2023-12-09 18:36:30 +0400

    Format code

--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -1,4 +1,3 @@
-
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,39 +8,65 @@ interface Props {
 }
 
 const schema = z.object({
-  description: z.string().min(3, { message: "Description should be at least 3 characters." }).max(50, { message: "Description should be maximum 50 characters." }),
-  amount: z.number({ invalid_type_error: "Amount is required." }).min(0.01).max(100_000),
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." })
+    .max(50, { message: "Description should be maximum 50 characters." }),
+  amount: z
+    .number({ invalid_type_error: "Amount is required." })
+    .min(0.01)
+    .max(100_000),
   category: z.enum(categories, {
     errorMap: () => ({
-      message: "Category is required."
-    })
-  })
+      message: "Category is required.",
+    }),
+  }),
 });
 
 type ExpenseFormData = z.infer<typeof schema>;
 
 const ExpenseForm = ({ onSubmit }: Props) => {
-
-  const { register, handleSubmit, reset, formState: { errors } } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
 
   return (
-    <form onSubmit={handleSubmit(data => {
-      onSubmit(data);
-      reset();
-    })}>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input {...register("description")} id="description" type="text" className="form-control" />
-        {errors.description && <p className="text-danger">{errors.description.message}</p>}
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input {...register("amount", { valueAsNumber: true })} id="amount" type="number" className="form-control" />
-        {errors.amount && <p className="text-danger">{errors.amount.message}</p>}
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
@@ -55,9 +80,13 @@ const ExpenseForm = ({ onSubmit }: Props) => {
             </option>
           ))}
         </select>
-        {errors.category && <p className="text-danger">{errors.category.message}</p>}
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button className="btn btn-primary" disabled={!isValid}>
+        Submit
+      </button>
     </form>
   );
 };
