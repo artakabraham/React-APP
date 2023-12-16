eb1efc41bbed9fca977e7ab8835a29521e1e1f12
Artak <abrahamyan@outlook.com>
2023-12-16 23:50:14 +0400

    Handling http errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import axios from "axios";
 
 interface User {
@@ -11,21 +8,24 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data))
+      .catch((error) => setError(error.message));
   }, []);
 
   return (
-    <ul>
-      {users.map((user) => (
-        <>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
           <li key={user.id}>{user.name}</li>
-        </>
-      ))}
-    </ul>
+        ))}
+      </ul>
+    </>
   );
 }
 
